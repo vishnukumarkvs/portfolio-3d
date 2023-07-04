4eaad36fc95028dff66f378232bb8f9f9731d003
vishnukumarkvs <kvsvishnukumar@gmail.com>
2023-07-04 17:26:45 +0530

    fixing build bugs

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
         </div>
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className="text-[#915EFF]">Vishnu</span>
+            Hi, I&apos;m <span className="text-[#915EFF]">Vishnu</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I am a Software Engineer <br className="sm:block hidden" />
